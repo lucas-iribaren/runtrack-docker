0c43773b75225bff106c040f0c1bd12d0d798dfb
lucas-iribaren <lucas.iribaren@laplateforme.io>
2025-02-21 11:30:31 +0100

    modified version of Lucy's container

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <header className="App-header">
         <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first container.
+          This is a modified version of Lucy's container - Welcome to Docker!
         </p>
         <div>
           <a
